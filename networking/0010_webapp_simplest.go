16be299e5a466571801690570e586de159b4ab88
nick avlo <avlo@yahoo.com>
2020-10-24 16:27:13 -0700

    simplest webapp

--- networking/0010_webapp_simplest.go
+++ networking/0010_webapp_simplest.go
@@ -7,8 +7,8 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
+	fmt.Fprintf(w, "Welcome to the HomePage!") // writes to ResponseWriter
+	fmt.Println("Endpoint Hit: homePage")      // writes to console
 }
 
 func handleRequests() {
