5a2f1bb1cd9f8a4d4c4b2188d446205d4697be52
nick avlo <avlo@yahoo.com>
2020-10-29 17:26:14 -0700

    added books db

--- fiber/book-example/book/book.go
+++ fiber/book-example/book/book.go
@@ -1,9 +1,23 @@
 package book
 
-import "github.com/gofiber/fiber"
+import (
+	"github.com/avlo/fiber/book-example/database"
+	"github.com/gofiber/fiber"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	gorm.Model
+	Title  string `json:"name"`
+	Author string `json:"author"`
+	Rating int    `json:"rating"`
+}
 
 func GetBooks(c *fiber.Ctx) error {
-	return c.SendString("All books")
+	dba := database.DBConn
+	var books []Book
+	dba.Find(&books)
+	return c.JSON(books)
 }
 
 func GetBook(c *fiber.Ctx) error {
--- fiber/book-example/main.go
+++ fiber/book-example/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/avlo/fiber/book-example/book"
 	"github.com/gofiber/fiber"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func setupRoutes(app *fiber.App) {
@@ -12,8 +16,20 @@ func setupRoutes(app *fiber.App) {
 	app.Delete("/book/:id", book.DeleteBook)
 }
 
+func initDatabase() {
+	var err error
+	db, err := gorm.Open(sqlite.Open("books.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+	db.AutoMigrate(&book.Book{})
+	fmt.Println("Table Created")
+}
+
 func main() {
 	app := fiber.New()
+	initDatabase()
 	setupRoutes(app)
 	app.Listen(":3000")
 }
